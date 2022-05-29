d447b7b769a72c799f4689272691b162ee8bc683
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-05-29 13:26:05 +0000

    [create]routes books

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :image
   
+  has_many :books, dependent: :destroy
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :books, only: [:index, :show]
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  get 'home/about' => 'homes#about', as: 'about'
+  get 'home/about' => 'homes#about'
 end
