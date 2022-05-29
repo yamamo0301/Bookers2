aa4ef1e2034e41ad65e546efe5182776d84dad78
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-05-29 09:46:22 +0000

    about create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :confingure_permitted_parameters, if: :devise_controller?
+    
+    def after_sign_in_path_for(resource)
+      about_path
+    end
 
-  protected
+    protected
 
-  def confingure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+    def confingure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
