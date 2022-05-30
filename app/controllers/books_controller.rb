371c97d788751eb0d8115af29bd69279ffacfb0f
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-05-30 12:39:49 +0000

    [create]Bootstrap

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,24 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
   def index
   end
 
   def show
+  end
+
+  provate
+
+  def post_image_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:index, :show]
+  resources :books, only: [:new,:index, :show]
   
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
