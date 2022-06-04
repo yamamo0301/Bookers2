585b110ff80ed59278a9de81c52c47ffacd7ca10
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-06-04 14:31:26 +0000

    [create]book_edit/book_update

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
 
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:new, :index, :show, :create, :edit, :destroy]
+  resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy]
 
 end
