a107ec89b56777f5954034b25d7b903207152cc1
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-06-04 15:53:52 +0000

    [created]flash_massege

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :confingure_permitted_parameters, if: :devise_controller?
 
     def after_sign_in_path_for(resource)
-      home_about_path
+      user_path(current_user.id)
     end
 
     protected
